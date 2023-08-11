64347a71b946521db6de6abf17990f5f1e7e59b4
piyushpanchal114 <piyushpanchal114@gmail.com>
2023-08-11 08:05:21 +0530

    Build Game Card

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,9 +3,10 @@ import apiClient from "../services/apiClient";
 import { CanceledError } from "axios";
 
 
-interface Game {
+export interface Game {
     id: number;
     name: string;
+    background_image: string;
   }
   
   interface FetchGamesResponse {
