072845f4e9888a135cec6dd23ede22eddb26e139
piyushpanchal114 <piyushpanchal114@gmail.com>
2023-08-28 23:04:00 +0530

    Filtering by Platforms

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,7 +1,6 @@
 import { Genre } from "./useGenres";
 import useData from "./useData";
 
-
 export interface Platform {
   id: number;
   name: string;
@@ -9,13 +8,20 @@ export interface Platform {
 }
 
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: {platform: Platform}[];
-    metacritic: number
-  }
-  
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
+}
 
-const useGames = (selectedGenre: Genre | null) =>  useData<Game>("/games", {params: {genres:selectedGenre?.id}}, [selectedGenre?.id])
-export default useGames
+const useGames = (
+  selectedGenre: Genre | null,
+  selectedPlatform: Platform | null
+) =>
+  useData<Game>(
+    "/games",
+    { params: { genres: selectedGenre?.id, platforms: selectedPlatform?.id } },
+    [selectedGenre?.id, selectedPlatform?.id]
+  );
+export default useGames;
