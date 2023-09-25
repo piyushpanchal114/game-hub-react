c7f670e7fdd526a1031066d1304f2fb9bcbba361
piyushpanchal114 <piyushpanchal114@gmail.com>
2023-09-25 21:40:44 +0530

    Fetch Genres using react query

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -2,36 +2,45 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/apiClient";
 import { AxiosRequestConfig, CanceledError } from "axios";
 
-
-interface FetchResponse <T>{
-    count: number;
-    results: T[];
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
 }
 
-const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {
-    const [data, setData] = useState<T[]>([]);
-    const [error, setError] = useState("");
-    const [isLoading, setLoading] = useState(false);
-  
-    useEffect(() => {
-        const controller = new AbortController();
+const useData = <T>(
+  endpoint: string,
+  requestConfig?: AxiosRequestConfig,
+  deps?: any[]
+) => {
+  const [data, setData] = useState<T[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(
+    () => {
+      const controller = new AbortController();
       setLoading(true);
       apiClient
-        .get<FetchResponse<T>>(endpoint, {signal: controller.signal, ...requestConfig})
+        .get<FetchResponse<T>>(endpoint, {
+          signal: controller.signal,
+          ...requestConfig,
+        })
         .then((res) => {
           setData(res.data.results);
           setLoading(false);
         })
         .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message); 
-        setLoading(false);
-      })
+          if (err instanceof CanceledError) return;
+          setError(err.message);
+          setLoading(false);
+        });
 
-        return () => controller.abort()
-    }, deps ? [...deps] : []);
+      return () => controller.abort();
+    },
+    deps ? [...deps] : []
+  );
 
-    return {data, error, isLoading}
-}
+  return { data, error, isLoading };
+};
 
 export default useData;
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,13 +1,20 @@
-import useData from "./useData";
-
+import { useQuery } from "@tanstack/react-query";
+import { FetchResponse } from "./useData";
+import apiClient from "../services/apiClient";
 
 export interface Genre {
-    id: number;
-    name: string;
-    image_background: string;
+  id: number;
+  name: string;
+  image_background: string;
 }
 
-
-const useGenres = () => useData<Genre>("/genres")
+const useGenres = () => {
+  return useQuery({
+    queryKey: ["genres"],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, //24 Hrs
+  });
+};
 
 export default useGenres;
