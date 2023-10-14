44d3b036907541a95bb532fa0997e9d22931c19f
piyushpanchal114 <piyushpanchal114@gmail.com>
2023-10-14 21:28:28 +0530

    FIxed the issue with layout

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -22,7 +22,11 @@ const GameGrid = () => {
         next={() => fetchNextPage()}
         loader={<Spinner />}
       >
-        <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={3}>
+        <SimpleGrid
+          columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+          padding="10px"
+          spacing={6}
+        >
           {isLoading &&
             skeletons.map((skeleton) => (
               <GameCardContainer key={skeleton}>
